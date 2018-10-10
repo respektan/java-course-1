b36ffa6bbb28ff2136c661fb50c29fb9c884022c
respektan <43926590+respektan@users.noreply.github.com>
2018-10-10 12:15:41 +0200

    kalkulator
    Nie dziala :(

---
+++ first.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class first extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        System.out.println( a + b );
+        System.out.println( a - b );
+        System.out.println( a / c );
+    }
+}

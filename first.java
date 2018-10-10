ea529bab1d8b1025f0f22148a12eec3523a993da
respektan <43926590+respektan@users.noreply.github.com>
2018-10-10 17:07:28 +0200

    Update first.java
    2 wersja

--- first.java
+++ first.java
@@ -19,3 +19,17 @@ public class App
 
     }
 }
+----------------------------------------------------------------------------------
+2 wersja 
+public class App 
+{
+    public static void main ( String[] args )
+{
+    int dodawanie = 2 + 2;
+    int odejmowanie = 2 - 2;
+    /*int dzielenie = 2 / 0;*/
+    System.out.println( dodawanie );
+    System.out.println( odejmowanie );
+    /*System.out.println( dzielenie ); */
+    }
+}
